2d01689bdc6b3822dc3d987098ae1a1659feab5a
Rizwan Kadiwal <rizwan.kadiwal@yahoo.ca>
2018-03-27 02:24:59 -0500

    Added Dashboard and applied bootstrap to product view

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+  layout "admin_application"
+
+  def admin
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :products
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/admin' do
+    resources :categories, :products
+    get '', to: 'dashboard#admin'
+  end
 end
